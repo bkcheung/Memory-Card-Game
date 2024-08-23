aa2d461331e506614c0d9f555bcc32579c951940
bkcheung <b.cheung@hotmail.com>
2024-08-22 22:33:47 -0400

    feat: add score and high score indicator

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,20 @@
-const Header = ()=>{
+interface headerProps{
+    score: number,
+    highScore: number
+}
+function Header({score, highScore}:headerProps){
     return(
         <>
-            <div className="title"></div>
-            <div className="pl-10 text-lg text-white">
-                CLICK ON EACH VILLAGER ONCE TO SCORE POINTS; NO REPEATS!</div>
+          <div className="flex items-center">
+            <div id="title"></div>
+            <div id="score" className="flex flex-col items-center justify-center text-white">
+                <div>Score: {score}</div>
+                <div>High Score: {highScore}</div>
+            </div>
+          </div>
+            <div className="pl-10 text-xl text-white"> 
+                Click on each villager once to score points; no repeats!
+            </div>
         </>
     )
 }
