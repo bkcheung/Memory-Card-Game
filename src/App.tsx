43fc4d29a6c6966d6646ca7981843a1a24a4b431
bkcheung <b.cheung@hotmail.com>
2024-08-22 22:43:08 -0400

    feat: add score and high score variables and save high score using localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react'
 import './index.css'
 import Header from './Header'
 
 function App() {
+  const storeScore = localStorage.getItem('highScore')?Number(localStorage.getItem('highScore')):0;
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(storeScore);
+
+  const incrementScore = ()=>{
+    const newScore = score + 1;
+    if(newScore>highScore)setHighScore(newScore);
+    setScore(newScore);
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('highScore', JSON.stringify(highScore))
+  },[highScore])
 
   return (
     <div id='page'> 
-      <Header></Header>
+      <Header score={score} highScore={highScore}></Header>
+      <button onClick={incrementScore}>Click</button>
     </div>
   )
 }
