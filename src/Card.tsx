dd7fee1912cd5cbaf0288d4a33c3ce1fa2084501
bkcheung <b.cheung@hotmail.com>
2024-08-27 16:24:24 -0400

    feat: update Card component

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,14 @@
 interface cardProps{
     score: ()=>void
+    vName: string
+    img: string
 }
-function Card({score}:cardProps){
+function Card({score, vName, img}:cardProps){
     return(
-        <button className="card" onClick={score}></button>
+        <button className="bg-white/50 rounded-xl flex flex-col items-center" onClick={score}>
+            <img src={img} className="h-5/6 p-4"></img>
+            <div>{vName}</div>
+        </button>
     )
 }
 
